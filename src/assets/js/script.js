b6b1c42f3e05b75d68a7f4963105d844e748a6aa
Elisei <eliseykrapivin@gmail.com>
2023-12-25 05:00:59 +0600

    third commit

---
+++ src/assets/js/script.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const slider = document.querySelector(".slider");
+  const slides = document.querySelectorAll(".slide");
+  const prevBtn = document.querySelector(".prev");
+  const nextBtn = document.querySelector(".next");
+
+  let currentIndex = 0;
+
+  function showSlide(index) {
+    if (index < 0) {
+      currentIndex = slides.length - 1;
+    } else if (index >= slides.length) {
+      currentIndex = 0;
+    } else {
+      currentIndex = index;
+    }
+
+    const translateValue = -currentIndex * 100 + "%";
+    slider.style.transform = "translateX(" + translateValue + ")";
+  }
+
+  function showNextSlide() {
+    showSlide(currentIndex + 1);
+  }
+
+  function showPrevSlide() {
+    showSlide(currentIndex - 1);
+  }
+
+  prevBtn.addEventListener("click", showPrevSlide);
+  nextBtn.addEventListener("click", showNextSlide);
+});
